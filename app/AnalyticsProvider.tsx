6d3bcf89deded6529760e2961fa11c1312b27188
Raibima Putra <raibima.putra@traveloka.com>
2024-10-27 19:40:40 +0700

    Disable analytics in DEV

--- app/AnalyticsProvider.tsx
+++ app/AnalyticsProvider.tsx
@@ -2,7 +2,9 @@
 import posthog from 'posthog-js';
 import {PostHogProvider} from 'posthog-js/react';
 
-if (typeof window !== 'undefined') {
+const IS_PROD = process.env.NODE_ENV === 'production';
+
+if (typeof window !== 'undefined' && IS_PROD) {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
@@ -13,5 +15,8 @@ export default function AnalyticsProvider({
 }: {
   children: React.ReactNode;
 }) {
-  return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
+  if (IS_PROD) {
+    return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
+  }
+  return children;
 }
