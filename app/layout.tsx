f7ff9e9c578454f8eee1aed14fac47752ffd3ad5
Raibima Putra <raibima.putra@traveloka.com>
2024-10-27 19:15:41 +0700

    Add posthog analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from 'next';
 import {SessionProvider} from 'next-auth/react';
 import localFont from 'next/font/local';
 import './globals.css';
+import AnalyticsProvider from './AnalyticsProvider';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -30,7 +31,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <SessionProvider>{children}</SessionProvider>
+        <AnalyticsProvider>
+          <SessionProvider>{children}</SessionProvider>
+        </AnalyticsProvider>
       </body>
     </html>
   );
